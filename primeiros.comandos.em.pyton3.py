72d205e419e11844352d36aad48b03881a0994f7
Domingosws <domingosw1991@gmail.com>
2022-01-05 21:23:31 -0300

    atualização 5

---
+++ primeiros.comandos.em.pyton3.py
@@ -0,0 +1,7 @@
+#Crie um script Python que leia o nome  idade, peso de uma pessoa e mostre uma mensagem de acordo com o valor digitado.
+nome = input('Qua é seu nome?')
+idade = input('Qual é sua idade?')
+peso = input('Qual é sue peso?')
+print( 'Seu nome é', nome)
+print('Sua idade é', idade)
+print('Seu peso é', peso)
