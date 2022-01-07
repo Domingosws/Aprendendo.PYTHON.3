979fd217fb21610db76eae8d4ea6d7a51160b935
Domingosws <domingosw1991@gmail.com>
2022-01-06 23:56:49 -0300

    exec.2

---
+++ exec.2.py
@@ -0,0 +1,6 @@
+#Tipos Primitivos e Saída de Dados
+#Crie um programa que lei dois numeros e mostre a soma entre ele.
+n1 = int(input('Digite um valor:'))
+n2 = int(input('Digite outro valor:'))
+s = n1 + n2
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
