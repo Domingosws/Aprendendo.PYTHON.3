0588f46c33461ec300fb060295a9fd637811666d
Domingosws <domingosw1991@gmail.com>
2022-01-10 23:45:05 -0300

    atualização

---
+++ exec.4.py
@@ -0,0 +1,3 @@
+ #Faça um programa que leiam um número inteiro e mostre na tela os seu sucessor e seu antecessor.
+n=int(input('\033[1;33mDigite um número:\033[m'))
+print('\033[1;34mAnalisando o valor{},seu antecerssor é {}, e o sussesor é {}'.format(n, (n-1), (n+1)))
