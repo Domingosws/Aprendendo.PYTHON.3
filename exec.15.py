e52a8a0740295902f1bf554a905bff46ff0847d9
Domingosws <domingosw1991@gmail.com>
2022-01-25 14:22:24 -0300

    Update exec.15.py

--- exec.15.py
+++ exec.15.py
@@ -2,4 +2,5 @@
 import math
 num = int(input('\033[1;31m Digite um numero? \033[m'))
 raiz = math.sqrt(num)
-print('\033[1;34m A raiz de {:.2f} é igual á {:.2f}'.format(num, raiz))
+#print('\033[1;34m A raiz de {:.2f} é igual á {:.2f}'.format(num, raiz))
+print('\033[1;34m A raiz de {} é igual á {}'.format(num, math.ceil(raiz)))#math.ceil()para redondar pra cima!
