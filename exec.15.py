dc6a584f4bd37e87f3ea1241c305f2a101bf92a2
Domingosws <domingosw1991@gmail.com>
2022-01-25 14:11:56 -0300

    Create exec.15.py

---
+++ exec.15.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia um número real qual quer pelo teclado e mostre na tela a sua porção inteira.
+import math
+num = int(input('\033[1;31m Digite um numero? \033[m'))
+raiz = math.sqrt(num)
+print('\033[1;34m A raiz de {:.2f} é igual á {:.2f}'.format(num, raiz))
