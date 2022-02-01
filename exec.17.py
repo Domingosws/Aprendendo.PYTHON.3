9f2197e1f09fe6eb0d6984beb88a6d715004e0b6
Domingosws <domingosw1991@gmail.com>
2022-02-01 00:09:31 -0300

    Update exec.17.py

--- exec.17.py
+++ exec.17.py
@@ -1,9 +1,10 @@
 #Seno, cosseno e tangente
 #Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, e tangente desse ângulo.
 import math
-ângulo=floot(input('Digite o ângulo que você deseja:'))
+ângulo=float(input('Digite o ângulo que você deseja:'))
 seno=math.sin(math.radians(ângulo))
-print('O angulo de {} tem o SENO de {}'.format(angulo,seno))
+print('O angulo de {} tem o SENO de {:.2f}'.format(ângulo,seno))
 cosseno=math.cos(math.radians(ângulo))
-print('O ângulo de {}tem o COSENO de {}'.format(ângulo,cosseno))
-tangente=
+print('O ângulo de {}tem o COSENO de {:.2f}'.format(ângulo,cosseno))
+tangente=math.tan(math.radians (ângulo))
+print('O ãngulo de {} tem a TANGENTE de {:.2f}'.format(ângulo,tangente))
