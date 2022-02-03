729b2219142610d27f177f8aab870c53ae3b348c
Domingosws <domingosw1991@gmail.com>
2022-02-03 00:11:25 -0300

    Update exec.18.py

--- exec.18.py
+++ exec.18.py
@@ -1,9 +1,11 @@
 #Continuaçãodo execicio 17
-form math import radians,sin,cos,tan
-ângulo=float(input('Digite o angulo que você deseja:'))
-seno=sin(radians(ângulo))
-print('O ãngulo de {} tem o SENO de {:.2f}'.format(ângulo,seno))
-cosseno=cos (radians(angulo))
+#Usando inportação
+from math import  radians,sin,cos,tan
+ângulo = float(input('\033[1;33mDigite o angulo que você deseja:\033[m'))
+seno = sin(radians(ângulo))
+print('\033[1;34mO ãngulo de {} tem o SENO de {:.2f}'.format(ângulo,seno))
+cosseno = cos (radians(ângulo))
 print('O ângulo de {} tem o COSSENO de {:.2f}'.format(ângulo,cosseno))
-tangente=tang(radians(ângulo))
-print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(ângulo, tangente))
+tangent = tan(radians(ângulo))
+print('O ângulo de {} tem a TANGENTE de {:.2f}\033[m'.format(ângulo, tangent))
+
