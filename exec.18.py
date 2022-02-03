ac9d59a98328161f22f1d377730b8d775eab5d91
Domingosws <domingosw1991@gmail.com>
2022-02-02 23:57:44 -0300

    Create exec.18.py

---
+++ exec.18.py
@@ -0,0 +1,9 @@
+#Continuaçãodo execicio 17
+form math import radians,sin,cos,tan
+ângulo=float(input('Digite o angulo que você deseja:'))
+seno=sin(radians(ângulo))
+print('O ãngulo de {} tem o SENO de {:.2f}'.format(ângulo,seno))
+cosseno=cos (radians(angulo))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(ângulo,cosseno))
+tangente=tang(radians(ângulo))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(ângulo, tangente))
