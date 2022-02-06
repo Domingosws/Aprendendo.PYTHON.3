205b8ecac3de4feb29b068d6cac690271dd83aab
Domingosws <domingosw1991@gmail.com>
2022-02-05 22:09:09 -0300

    Update exec.20.py

--- exec.20.py
+++ exec.20.py
@@ -2,7 +2,7 @@
 #Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteando.
 import random#Metodos dentro de modulos
 n1=str(input('\033[1;33mPrimeiro aluno:\033[m'))
-n2=str(input('\033[1;34mSegundo aluno\033[m'))
+n2=str(input('\033[1;34mSegundo aluno:\033[m'))
 n3=str(input('\033[1;32mTerceio aluno:\033[m'))
 n4=str(input('\033[1;35mQarto aluno:\033[m'))
 lista=[n1,n2,n3,n4]
