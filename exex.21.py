104a555f0597cf9bfad31d65c1d77687ba4ce33f
Domingosws <domingosw1991@gmail.com>
2022-02-06 22:05:58 -0300

    Create exex.21.py

---
+++ exex.21.py
@@ -0,0 +1,9 @@
+#Analisador de textos - Manipulando texto
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+#O nome com todas as letras maiusculas e minúsculas.
+#Quantas letras ao todo(sem considerar espaços).
+#Quantas letras tem o primeiro nome.
+nome=str(input('Digite seu nome completo:'))
+print('Analisando seu nome...')
+print('Seu nome em maiúsculas é {}'.format(nome.upper()))
+
