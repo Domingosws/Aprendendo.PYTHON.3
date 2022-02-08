9068a4b97bef72f3c8dd2ffaa5b189c03a1d8b36
Domingosws <domingosw1991@gmail.com>
2022-02-07 23:57:47 -0300

    Create exec.22.py

---
+++ exec.22.py
@@ -0,0 +1,4 @@
+#Analisador de textos-Crie um programa que leia o nome completo de uma pessoa e mostre:
+#O nome com todas letras maiusculas e ,inúsculas.
+#Quantas letras ao todo (sem considerar espaços).
+#Quantas letras tem o primeiro nome.
