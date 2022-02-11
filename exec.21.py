a63710f540dccb20aa4b38736377b733f8409399
Domingosws <domingosw1991@gmail.com>
2022-02-10 23:54:49 -0300

    Update exec.21.py

--- exec.21.py
+++ exec.21.py
@@ -1,4 +1,4 @@
-#Analizandor de textos
+#Analizandor de textos 
 #Crie um programa que leia o nome completo de uma pessoa e mostre:
 #-o nome com todas as letras ,aiusculas e minúsculas.
 #-Quantas letras ao todo (sem considerar espaços).
@@ -7,4 +7,5 @@ nome=str(input('Digite seu nome completo:')).strip()
 print('Analisando seu nome...')
 print('Seu nome em maiúsculas é {}'.format(nome.upper()))
 print('Seu nome em minúsculas é{}'.format(nome.lower()))
-print('Seu nome tem ao todo {}'.format(len(nome)-nome.count()))
+print('Seu nome tem ao todo {}'.format(len(nome)-nome.count('')))
+nome.find('')
