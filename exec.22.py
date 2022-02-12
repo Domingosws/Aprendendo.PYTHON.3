b918ed626d4e45ebe24c64f8cb94e25103a227a7
Domingosws <domingosw1991@gmail.com>
2022-02-11 23:51:42 -0300

    Update exec.22.py

--- exec.22.py
+++ exec.22.py
@@ -1,4 +1,1 @@
-#Analisador de textos-Crie um programa que leia o nome completo de uma pessoa e mostre:
-#O nome com todas letras maiusculas e ,inúsculas.
-#Quantas letras ao todo (sem considerar espaços).
-#Quantas letras tem o primeiro nome.
+#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
