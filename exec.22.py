09c75a0f64e71ce996f26e94b7e705b27716de35
Domingosws <domingosw1991@gmail.com>
2022-02-13 22:28:03 -0300

    Update exec.22.py

--- exec.22.py
+++ exec.22.py
@@ -1,1 +1,11 @@
 #Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+num=int(input('Informe um numero:'))
+u= num // 1 % 10
+d= num // 10 % 10
+c= num // 100 % 10
+m= num // 100 % 10
+print('Analizado o número {}'.format(num))
+print('Unidade:{}'.format(u))
+print('Dezena:{}'.format(d))
+print('centena:{}'.format(c))
+print('Milhar:{}'.format(m))
