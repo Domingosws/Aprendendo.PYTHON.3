13c2ccddc6d6b871b21b93de7416034434e8ea74
Domingosws <domingosw1991@gmail.com>
2022-02-16 13:31:41 -0300

    Update exec.21.py

--- exec.21.py
+++ exec.21.py
@@ -3,10 +3,11 @@
 #-o nome com todas as letras ,aiusculas e minúsculas.
 #-Quantas letras ao todo (sem considerar espaços).
 #-Quantas letras tem o primeiro nome.
-nome=str(input('\033[1;31mDigite seu nome completo:\033[m')).strip()
-print('Analisando seu nome...')
-print('Seu nome em maiúsculas é {}'.format(nome.upper()))
+nome = str(input('\033[1;33mDigite seu nome completo:')).strip()
+print('Analisando seu nome...\033[m')
+print('\033[1;35mSeu nome em maiúsculas é {}'.format(nome.upper()))
 print('Seu nome em minúsculas é {}'.format(nome.lower()))
-print('Seu nome tem ao todo {}'.format(len(nome) - nome.count('')))
-print('Seu nome tem {} letras'.format(nome.find('')))
-
+print('Seu nome tem ao todo {}'.format(len(nome) - nome.count(' ')))
+#print('Seu nome tem {} letras'.format(nome.find(' ')))
+separa=nome.split()#Segunda maneira de fazer!
+print('Seu primeiro nome é {} ele tem {} letras'.format(separa[0],len(separa[0])))
