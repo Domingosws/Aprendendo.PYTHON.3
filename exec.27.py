d0afd18b2ad8943d8704b3073086c18041e03e73
Domingosws <domingosw1991@gmail.com>
2022-02-23 23:41:55 -0300

    Update exec.27.py

--- exec.27.py
+++ exec.27.py
@@ -1,3 +1,7 @@
 #Codições como utilizar estruturas condicionais simplese compostas nos seus programas.
 #se -> if ->senão -> else
 #condição simples
+nome=str(input('Qual é seu nome?'))
+if nome=='Domingos':
+    print('Que nome lindo vc tem!')
+print('Bua noite, {}'.format(nome))
