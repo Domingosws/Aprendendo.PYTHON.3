8a028b5d582e3a5436e9730f0a5e168d2275ccf1
Domingosws <domingosw1991@gmail.com>
2022-02-24 23:40:50 -0300

    Create exec.28.py

---
+++ exec.28.py
@@ -0,0 +1,7 @@
+#condição composta
+nome=str(input('Qual é seu nome?'))
+if nome=='domingos':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão nomal!')
+print('Boa noite, {}'.format(nome))
