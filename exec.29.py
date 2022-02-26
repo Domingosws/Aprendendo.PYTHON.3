5a0ea542ecf58ac4ad0cf842495b93348aef7c9d
Domingosws <domingosw1991@gmail.com>
2022-02-25 23:43:59 -0300

    exec.29
    jogo da adivinhação

---
+++ exec.29.py
@@ -0,0 +1,3 @@
+#Jogo da adivinhação
+#Escreva um programa que faça o computador, 'pensar' em um número inteiro entre 0 e 5 e peça para o usário tentar
+#descobrir qual foi o número escolhido pelo computador: O programa derá escrever na tela se o usuário venceu ou perdeu.
