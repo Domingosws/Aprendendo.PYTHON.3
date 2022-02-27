2114f15e676dd5ddb630b8aee93e766ab044a7db
Domingosws <domingosw1991@gmail.com>
2022-02-26 23:48:03 -0300

    Update exec.29.py

--- exec.29.py
+++ exec.29.py
@@ -1,3 +1,6 @@
 #Jogo da adivinhação
 #Escreva um programa que faça o computador, 'pensar' em um número inteiro entre 0 e 5 e peça para o usário tentar
 #descobrir qual foi o número escolhido pelo computador: O programa derá escrever na tela se o usuário venceu ou perdeu.
+from random import randint
+computador=randint(0,5)#Faz o computador "PESSAR# "
+print('-=-'*20)
