873891dbcdbb688dabca369c63ff6abb7b7c542e
Domingosws <domingosw1991@gmail.com>
2022-03-01 22:56:50 -0300

    Update exec.29.py

--- exec.29.py
+++ exec.29.py
@@ -1,12 +1,16 @@
 #Jogo da adivinhação
 #Escreva um programa que faça o computador, 'pensar' em um número inteiro entre 0 e 5 e peça para o usário tentar
 #descobrir qual foi o número escolhido pelo computador: O programa derá escrever na tela se o usuário venceu ou perdeu.
-from random import randint
+from random import randint#inportação de biblioteca
+from time import sleep
 computador=randint(0,5)#Faz o computador "PESSAR# "
 print('-=-'*20)
-print('Vou pesar em um núméro entre 0 e 5 , tente adivinhar')
+print('\033[1;32mVou pesar em um núméro entre 0 e 5 , tente adivinhar...\033[m')
 print('-=-'*20)
-jogador=int('Ém que número eu pensei?')
+jogador=int(input('\033[1;34mEm que numero eu pensei?\033[m'))#Jogador tenta adivinhar
+print('\033[1;31mPROCESSANDO...\033[m')
+sleep(3)
 if jogador==computador:
-    print('PARABÉNS! Você conceguiu me vencer!')
-    
+    print('\033[1;33mPARABÉNS! Você conseguiu me vencer!\033[m')
+else:
+    print('\033[1;35mGANHEI! Eu pensei no número {} e não no {}!\033[m'.format(computador,jogador))
