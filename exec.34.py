db5e180cdaf1c58e8a5e995e3530e497fed5929e
Domingosws <domingosw1991@gmail.com>
2022-03-07 23:58:09 -0300

    Update exec.34.py

--- exec.34.py
+++ exec.34.py
@@ -1,2 +1,17 @@
 #Maior e menor valores
 #Faça um programaa que leia três numeros e mostre qual é o maior e qual é o menor.
+a=int(input('\033[1;32mPrimeiro valor:'))
+b=int(input('Segundo valor:'))
+c=int(input('Terceiro valor:\033[m'))
+#Verificando quem é menor
+menor = a
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+    menor = c
+print('\033[1;31mO menor valor digitado foi {}\033[m'.format(menor))
+#verificando quem é maior
+maior = a
+if b>a and a>c:
+    maior = b
+    
