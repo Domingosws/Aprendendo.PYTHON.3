fc822ad45b7f2dd5515b7445d5e954deb5fe2ac9
Domingosws <domingosw1991@gmail.com>
2022-03-12 19:36:06 -0300

    Update exec.36.py

--- exec.36.py
+++ exec.36.py
@@ -1,2 +1,8 @@
 #Analisando Triângulo
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+print('-*-'*20)
+print('Analisando Triângulo')
+print('-*-'*20)
+r1 = float(input('Primeiro segmento:'))
+r2 = float(input('Segundo segmento:'))
+r3 = float(input('Terceiro segmento:'))
