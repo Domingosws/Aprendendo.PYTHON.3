6b98443fc3ba778e23996345016af5db7d5e6010
Domingosws <domingosw1991@gmail.com>
2022-03-17 19:36:31 -0300

    Update exec.37.py

--- exec.37.py
+++ exec.37.py
@@ -1,2 +1,4 @@
 # Condições em Pyton(if.elif)
-#Aprovando Empréstismo
+#Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+#Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+#A prestação mensal, não pode execeder 30% do salário ou então o emprestimo sera negado.
