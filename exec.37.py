94c5ef1a99f75a16cb5dd2373106eb0c952cdc79
Domingosws <domingosw1991@gmail.com>
2022-03-17 19:53:20 -0300

    Update exec.37.py

--- exec.37.py
+++ exec.37.py
@@ -1,4 +1,12 @@
-# Condições em Pyton(if.elif)
+# Condições em Pyton(if.elif).
+#Aprovando Empréstimo.
 #Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
 #Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 #A prestação mensal, não pode execeder 30% do salário ou então o emprestimo sera negado.
+casa = float(input('Valor da casa: R$'))
+salário = float(input('Salario do comprador: R$'))
+anos = int(input('Quantos anos de finaciamento?'))
+prestação = casa / (anos * 12)
+mínimo = salário * 30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end =' ')
+print('A prestação será de R${:.2f}'.format(prestação))
