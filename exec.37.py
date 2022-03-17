be7a5df821d627783e60a683a1095943abdd4e77
Domingosws <domingosw1991@gmail.com>
2022-03-17 19:58:39 -0300

    Update exec.37.py

--- exec.37.py
+++ exec.37.py
@@ -10,3 +10,7 @@ prestação = casa / (anos * 12)
 mínimo = salário * 30 / 100
 print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end =' ')
 print('A prestação será de R${:.2f}'.format(prestação))
+if prestação <= mínimo:
+    print('Emprestimo pode ser CONCEDIDO!')
+else:
+    print('Emprestimo NEGADO!')
