45d65c4823dc18975f98a61184be611acdd84451
Domingosws <domingosw1991@gmail.com>
2022-03-18 19:42:03 -0300

    Update exec.38.py

--- exec.38.py
+++ exec.38.py
@@ -6,3 +6,12 @@ print('''Escolha uma das bases para conversão:
 [ 1 ] Converter para BINÁRIO
 [ 2 ] Converter para OCTAL
 [ 3 ] Converter para HEXADECIMAL''')
+opção = int(input('Sua opção:'))
+if opção == 1:
+    print('{} Convertido para BINÁRIO é igual a {}'.format(num, bin(num)[2:]))
+elif opção == 2:
+    print('{} Convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
+elif opção == 3:
+    print('{} Convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else:
+    print('Opção invalida. Tente novamente.')
