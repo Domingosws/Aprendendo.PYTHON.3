e8a6feea0d301acd333a0fd96c66ba39be29090f
Domingosws <domingosw1991@gmail.com>
2022-03-18 18:39:42 -0300

    Create exec.38.py

---
+++ exec.38.py
@@ -0,0 +1,3 @@
+#Execício 37 conversor de bases de numéricas
+#Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de
+#conversão: 1 para binario, 2 para octal e 3 para hexadecimal.
