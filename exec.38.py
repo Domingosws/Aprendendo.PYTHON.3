e4df299c1bbfe63e1bb9ffbfad3173d8cc874967
Domingosws <domingosw1991@gmail.com>
2022-03-18 19:23:54 -0300

    Update exec.38.py

--- exec.38.py
+++ exec.38.py
@@ -1,3 +1,8 @@
 #Execício 37 conversor de bases de numéricas
 #Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de
 #conversão: 1 para binario, 2 para octal e 3 para hexadecimal.
+num = int(input('Digite um número inteiro:'))
+print('''Escolha uma das bases para conversão:
+[ 1 ] Converter para BINÁRIO
+[ 2 ] Converter para OCTAL
+[ 3 ] Converter para HEXADECIMAL''')
