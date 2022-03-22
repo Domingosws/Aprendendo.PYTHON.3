912d14ac208c321bbab0d993705f257df6056775
Domingosws <domingosw1991@gmail.com>
2022-03-22 20:24:36 -0300

    Update exec.39.py

--- exec.39.py
+++ exec.39.py
@@ -3,11 +3,11 @@
 #Oprimeiro valor é maior
 #O Segundo valor e maior
 #Não existe valor maior, os dois são iguais
-n1 = int(input('Primeiro número:'))
-n2 = int(input('Segundo número:'))
+n1 = int(input('\033[1;34mPrimeiro número:\033[m'))
+n2 = int(input('\033[1;33mSegundo número:\033[m'))
 if n1 > n2:
-    print('O PRIMEIRO valor é maior')
+    print('\033[1;34mO PRIMEIRO valor é maior\033[m')
 elif n2 > n1:
-    print('O SEGUNDO valor é maior')
+    print('\033[1;33mO SEGUNDO valor é maior\033[m')
 else:
-    print('Os dois valores são IGUAIS')
+    print('\033[1;4;31mOs dois valores são IGUAIS\033[m')
