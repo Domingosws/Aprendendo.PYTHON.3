ff644c02be600a773d374de6d1985450976548fe
Domingosws <domingosw1991@gmail.com>
2022-03-23 11:35:14 -0300

    Create exec.40.py

---
+++ exec.40.py
@@ -0,0 +1,3 @@
+#Faça um programa que leia o nacimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se
+#alistarao serviço militar, se é a hora exata de se alista ou se já passou do tempo do alistamento. Seu programa também
+#deverá mostra o tempo que flata ou que passou do prazo.
