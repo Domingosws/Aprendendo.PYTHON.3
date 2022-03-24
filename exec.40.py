b4c52944b64e079f9f75dbb7316d8b9f70064904
Domingosws <domingosw1991@gmail.com>
2022-03-23 22:41:11 -0300

    Update exec.40.py
    Alistamento Militar

--- exec.40.py
+++ exec.40.py
@@ -3,18 +3,18 @@
 # Seu programa também deverá mostrar o tempo que falta ou que passou do prazo
 from datetime import date
 atual = date.today().year
-nasc = int(input('\033[1;32mAno dae nacimento:\033[m'))
+nasc = int(input('\033[1;32mAno de nacimento: \033[m'))
 idade = atual - nasc
-print('\033[1;33mQuem nasceu em {} tem {} ano em {}\033[m'.format(nasc, idade, atual))
+print('\033[1;33mQuem naceu em  {} tem {} anos em {}. \033[m'.format(nasc, idade, atual))
 if idade == 18:
-    print('\033[1;34mVocê tem que se alistar INEDIATAMENTE!\033[m')
+    print('\033[1;35mVocê tem que se alistar\033[m \033[1;4;31mIMEDIATAMENTE!\033[m')
 elif idade < 18:
-    print('\033[1;36mAinda faltam {} anos para o alistamento\033[m'.format(saldo))
-    ano = atual + saldo
     saldo = 18 - idade
-    print('\033[1;35mSeu aleistamento será em {}\033m'.format(ano))
-elif idade > 18:  # poderia ser também -> else:
+    print('\033[1;36mAida faltam {} anos para se o alistamento.\033[m'.format(saldo))
+    ano = atual + saldo
+    print('\033[1;34mSeu alistamento será em {} ano.\033[m'.format(ano))
+elif idade > 18:#Poderia ser também -> else:
     saldo = idade - 18
-    print('\033[1;31mvocê ja deveria ter se alistado ha {} anos\033[m'.format(saldo))
+    print('\033[1;37mVocê ja deveria ter se alistado há {}. \033[m'.format(saldo))
     ano = atual - saldo
-    print('\033[1;37mSeu alistamento foi em {}\033[m'.format(ano))
+    print('\033[1;34mSeu alistamento foi em {}. \033[m'.format(ano))
