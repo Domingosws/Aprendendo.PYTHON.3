6c8398dfb8fb6215106fd6ede1a31c0559184a56
Domingosws <domingosw1991@gmail.com>
2022-03-23 21:24:29 -0300

    Update exec.40.py

--- exec.40.py
+++ exec.40.py
@@ -1,6 +1,6 @@
-#Faça um programa que leia o nacimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se
-# alistarao serviço militar, se é a hora exata de se alista ou se já passou do tempo do alistamento. Seu programa também
-# deverá mostra o tempo que flata ou que passou do prazo.
+#Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade,
+# se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento.
+# Seu programa também deverá mostrar o tempo que falta ou que passou do prazo
 from datetime import date
 atual = date.today().year
 nasc = int(input('\033[1;32mAno dae nacimento:\033[m'))
@@ -9,9 +9,10 @@ print('\033[1;33mQuem nasceu em {} tem {} ano em {}\033[m'.format(nasc, idade, a
 if idade == 18:
     print('\033[1;34mVocê tem que se alistar INEDIATAMENTE!\033[m')
 elif idade < 18:
-    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    print('\033[1;36mAinda faltam {} anos para o alistamento\033[m'.format(saldo))
     ano = atual + saldo
-    print('Seu aleistamento será em {}'.format(ano))
+    saldo = 18 - idade
+    print('\033[1;35mSeu aleistamento será em {}\033m'.format(ano))
 elif idade > 18:  # poderia ser também -> else:
     saldo = idade - 18
     print('\033[1;31mvocê ja deveria ter se alistado ha {} anos\033[m'.format(saldo))
