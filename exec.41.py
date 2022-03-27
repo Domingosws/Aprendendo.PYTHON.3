5a245f7189031840d58abc6e2a78f4398a9d19ad
Domingosws <domingosw1991@gmail.com>
2022-03-26 23:12:27 -0300

    Update exec.41.py
    Aquele clássico da Média

--- exec.41.py
+++ exec.41.py
@@ -3,4 +3,13 @@
 # Média abaixo de 5.0: REPROVADO
 # Média entre 5.0 e 6.9: RECUPERAÇÃO
 # Média 7.0 ou superior: APROVADO
-media=
+nota1 = float(input('\033[1;32mPrimeira nota:\033[m'))
+nota2 = float(input('\033[1;32mSegunda nota:\033[m'))
+média = (nota1 + nota2) / 2
+print('\033[1;35mTirando {:.1f} e {:.1f}, a média do aluno é {:.1f}\033[m'.format(nota1, nota2, média))
+if média >=5 and média < 7:# Ou if 7 > media >=5:
+    print('\033[1;31mO aluno está em RECUPERAÇÃO.\033[m')
+elif média <=5:
+    print('\033[1;31mO aluno está REPROVADO\033[m')
+else: #Ou elif média > = 7:
+    print('\033[1;34mO aluno está APROVADO.\033[m')
