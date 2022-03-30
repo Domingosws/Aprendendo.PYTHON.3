e48e6320392b8deb7380a161777ee314e1f2bfe1
Domingosws <domingosw1991@gmail.com>
2022-03-29 23:56:58 -0300

    Update exec.42.py

--- exec.42.py
+++ exec.42.py
@@ -6,4 +6,18 @@
 # – Até 19 anos: JÚNIOR
 # – Até 25 anos: SÊNIOR
 # – Acima de 25 anos: MASTER
-#
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Ano de Nacimento: '))
+idade = atual - nascimento
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Clasificação: MIRIM')
+elif idade >9 and idade <= 14:
+    print('Clasificação:INFANTIL')
+elif idade <= 19:
+    print('Clasificação:JÚNIOR')
+elif idade <= 25:
+    print('clasificação:SÊNIOR')
+else:
+    print('Clasificação:MASTER')
