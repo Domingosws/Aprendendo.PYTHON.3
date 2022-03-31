3a866a9a00906b7b0641f56072593f4c83271a88
Domingosws <domingosw1991@gmail.com>
2022-03-31 18:08:48 -0300

    Update exec.43.py
    Analisando Triângulos

--- exec.43.py
+++ exec.43.py
@@ -3,3 +3,16 @@
 # – EQUILÁTERO: todos os lados iguais
 # – ISÓSCELES: dois lados iguais, um diferente
 # – ESCALENO: todos os lados diferentes
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 +r2:
+    print('Os segmentos acimam PODEM FARMAR um triagulo', end=' ')
+    if r1 == r2 == r3:
+        print('EQUILATERO!')
+    elif r1 != r2 != r3!= r1:
+        print('ESCALENO!')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Os segmentos acima NÃO PODEM FORMA um triagulo')
