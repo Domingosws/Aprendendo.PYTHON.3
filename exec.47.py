06c4399c4e6b147caa72d4a86415157b2aa11eee
Domingosws <domingosw1991@gmail.com>
2022-04-12 00:18:34 -0300

    Update exec.47.py

--- exec.47.py
+++ exec.47.py
@@ -1,2 +1,3 @@
 #Estruturas de Repetição.
-# fazendo...
+#Faça um programa que mostre na tela uma contagem regressiva para o estouo de fogos de artifício, indo de 10 até 0
+# com uma pausa de 1 segundo entre eles.
