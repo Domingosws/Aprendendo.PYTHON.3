d7ee18b653158af73d719179c9105180669cd1d1
Domingosws <domingosw1991@gmail.com>
2022-04-14 20:32:03 -0300

    Update exec.48.py

--- exec.48.py
+++ exec.48.py
@@ -1,2 +1,5 @@
 #Contagem de pares
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+for n in range (1, 50, 2):
+     print(n, end=' ')
+print('\033[1;31mAcabou\033[m')
