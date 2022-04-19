d1d7c409755ccd34b49bf3409bcb055e67eda986
Domingosws <domingosw1991@gmail.com>
2022-04-19 18:00:00 -0300

    Update exec.49.py

--- exec.49.py
+++ exec.49.py
@@ -1,2 +1,9 @@
 #Soma ímpares múltiplos de três.
 #faça um programa que calcule a soma entre todos os numeros que são mútiplos de trê eque se encantram no intevalo de 1 até 500.
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont = cont + 1
+        soma = soma + c
+print('A soma de todos os valores {} solicitados é {}'.format(cont, soma))
