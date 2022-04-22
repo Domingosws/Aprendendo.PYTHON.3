04dafed2d5fc281bb0b06cee39183162eeaed8a0
Domingosws <domingosw1991@gmail.com>
2022-04-21 23:31:52 -0300

    Update exec.51.py

--- exec.51.py
+++ exec.51.py
@@ -1,2 +1,9 @@
 #soma dos pares
 #mostre a soma apenas daqueles que forem pares.Se o valor digitado for impar,desconsidere-o.
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Digite o {} valor: '.format(c)))
+    soma += num
+    cont += 1
+print('Você informou {} números e a soma foi {}'.format(cont, soma))
