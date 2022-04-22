7415f72a993dc4b22e4000807fbc95e48c197457
Domingosws <domingosw1991@gmail.com>
2022-04-22 00:04:23 -0300

    Update exec.51.py
    Soma dos pares

--- exec.51.py
+++ exec.51.py
@@ -4,6 +4,7 @@ soma = 0
 cont = 0
 for c in range(1, 7):
     num = int(input('Digite o {} valor: '.format(c)))
-    soma += num
-    cont += 1
+    if num % 2 == 0:
+        soma += num
+        cont += 1
 print('Você informou {} números e a soma foi {}'.format(cont, soma))
