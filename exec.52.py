53d4fc18d2f749ad26dde2bc5a8af488d781da2b
Domingosws <domingosw1991@gmail.com>
2022-04-23 00:19:31 -0300

    Update exec.52.py

--- exec.52.py
+++ exec.52.py
@@ -1,3 +1,9 @@
 #Progressão Aritmética
 #Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No
 #final, mostre os 10 primeros termos dessa progreção.
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+décimo= primeiro + (10 - 1) * razão
+for c in range(primeiro, décimo, razão):
+    print('{}'.format(c), end=' ➪ ')
+print('ACABOU')
