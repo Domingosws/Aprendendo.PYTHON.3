125a23f7352a72602ef05e5f0ea19d9f6d06b124
Domingosws <domingosw1991@gmail.com>
2022-04-25 15:04:10 -0300

    Update exec.53.py

--- exec.53.py
+++ exec.53.py
@@ -1,2 +1,9 @@
 #Números primos
 #Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+núm = int(input('Digite um numero: '))
+for c in range(1, núm +1):
+    if núm % c == 0:
+    print(' 'end='')
+else:
+    print('')
+print('')
