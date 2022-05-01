73d3ee1adb968a66c7e8cfd7cf3ce57db9a8fd9d
Domingosws <86332803+Domingosws@users.noreply.github.com>
2022-04-30 22:01:35 -0300

    Update exec.54.py

--- exec.54.py
+++ exec.54.py
@@ -1,2 +1,4 @@
-#Detector de Palíndromo
-asdadada
+# Detector de Palíndromo
+# crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+# Exemplos de palíndromos:
+
