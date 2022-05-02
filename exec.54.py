b66d6fdd047938788582fb8ff93e43c4c7e71c1b
Domingosws <domingosw1991@gmail.com>
2022-05-01 23:59:46 -0300

    Update exec.54.py
    Detector de palíndromo

--- exec.54.py
+++ exec.54.py
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
 #Detector de Palíndromo
 #Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
 # Exemplos de palíndromos:
-frase = str(input('\033[1;32mDigite uma frase: \033[m')).strip().upper()
-palavras = frase.split()
-junto = ''.join(palavras)
-inverso = ''
-for letra in range(len(junto) -1, -1, -1):
-    inverso += junto[letra]
+frase = str(input('\033[1;32mDigite uma frase: \033[m')).strip().upper()# Lendo a frase.
+palavras = frase.split()#Aqui foi separado em um  vetor -> numa lista.
+junto = ''.join(palavras)#Aqui eu juntei tudo numa so string.
+inverso = ''# 2 forma pode tira isso!
+inverso = junto[::-1]#2 forma.
+"""for letra in range(len(junto) -1, -1, -1):#Aqui foi feito o inverso dela.
+    inverso += junto[letra]"""#Outra forma de fazer é o de fatiamento.
+print('\033[1;32mO inverso de {} é {}\033[m'.format(junto, inverso))
 if inverso == junto:
     print('\033[1;32mTemos um palíndromo!\033[m')
 else:
     print('\033[1;32mA frase digitada não é um palindromo!\033[m')
-=======
-# Detector de Palíndromo
-# crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
-# Exemplos de palíndromos:
-frase = str(input('Digite uma frase:')).strip().upper()
-palavras = frase.split()
-junto =''.join(palavras)
-inverso = ''
-for letra in range(len(junto) - 1, -1, -1):
-    inverso += junto[letra]
-print('O inverso de {} é {}'.format(junto, inverso))
-if inverso == junto:
-    print('Temos um palíndromo!')
-else:
-    print('A frase digitada não é um palíndromo!')
-
-
->>>>>>> 18931f1f8516e59930f62ee969e5d3840978c9da
