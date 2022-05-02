ed9006a96022cd3e58d6142414db033ff3dfebbb
Domingosws <domingosw1991@gmail.com>
2022-05-01 23:25:48 -0300

    Update exec.54.py

--- exec.54.py
+++ exec.54.py
@@ -1,2 +1,13 @@
 #Detector de Palíndromo
-asdadada
+#Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+# Exemplos de palíndromos:
+frase = str(input('\033[1;32mDigite uma frase: \033[m')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+if inverso == junto:
+    print('\033[1;32mTemos um palíndromo!\033[m')
+else:
+    print('\033[1;32mA frase digitada não é um palindromo!\033[m')
