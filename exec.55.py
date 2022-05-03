26c392f6c1282303a9c8fa04a482a17d41dc08a7
Domingosws <domingosw1991@gmail.com>
2022-05-03 18:15:22 -0300

    Update exec.55.py

--- exec.55.py
+++ exec.55.py
@@ -6,13 +6,13 @@ atual = date.today().year
 totmaior = 0
 totmenor = 0
 for pess in range(1, 8):
-    nasc = int(input('\033[1;32mEm que ano a {} pessoa naceu? \033[m'.format(pess)))
+    nasc = int(input('\033[1;32mEm que ano a {} pessoa naceu?\033[m'.format(pess)))
     idade = atual -nasc
     if idade >= 21:
         totmaior += 1
-        print('Essa pessoa tem {} anos'.format(idade))
+        print('\033[1;32mEssa pessoa tem {} anos!\033[m'.format(idade))
     else:
         tomamenor += 1
-        print('Essa pessoa é menor')
-print('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
-print('E tambem tivermos {} pessoas menores de idade'.format(totmenor))
+        print('\033[1;32mEssa pessoa é menor\033[m')
+print('\033[1;32mAo todo tivemos {} pessoas maiores de idade\033[m'.format(totmaior))
+print('\033[1;32mE tambem tivermos {} pessoas menores de idade\022[m'.format(totmenor))
