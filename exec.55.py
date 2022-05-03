8e89026e3b1235735dcb2ff45951735bd2c6e154
Domingosws <domingosw1991@gmail.com>
2022-05-03 17:42:39 -0300

    Update exec.55.py

--- exec.55.py
+++ exec.55.py
@@ -1,3 +1,8 @@
 #Grupo da Maioridade.
 #Crie um programa que leia o ano de nascimento de sete pessoas.
 #mostre quantas ainda não atingirama maioridade e quantas já são maiores.
+from datetime import date
+atual = date.today().year
+nasc = int(input('\033[1;32mEm que ano a pessoa naceu? \033[m'))
+idade = atual -nasc
+print('Essa pessoa tem {} anos'.format(idade))
