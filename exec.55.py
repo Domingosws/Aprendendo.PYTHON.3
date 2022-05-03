06fec83e9e8c2c7aed0008a8d7f944e0e0742d98
Domingosws <domingosw1991@gmail.com>
2022-05-03 18:09:20 -0300

    Update exec.55.py
    Grupo da Maioridade

--- exec.55.py
+++ exec.55.py
@@ -3,6 +3,16 @@
 #mostre quantas ainda não atingirama maioridade e quantas já são maiores.
 from datetime import date
 atual = date.today().year
-nasc = int(input('\033[1;32mEm que ano a pessoa naceu? \033[m'))
-idade = atual -nasc
-print('Essa pessoa tem {} anos'.format(idade))
+totmaior = 0
+totmenor = 0
+for pess in range(1, 8):
+    nasc = int(input('\033[1;32mEm que ano a {} pessoa naceu? \033[m'.format(pess)))
+    idade = atual -nasc
+    if idade >= 21:
+        totmaior += 1
+        print('Essa pessoa tem {} anos'.format(idade))
+    else:
+        tomamenor += 1
+        print('Essa pessoa é menor')
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
+print('E tambem tivermos {} pessoas menores de idade'.format(totmenor))
