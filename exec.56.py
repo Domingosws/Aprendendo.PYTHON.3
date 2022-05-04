99097a6d828ccd26b3fbd155ab5a320cc47c5f5f
Domingosws <domingosw1991@gmail.com>
2022-05-04 17:29:51 -0300

    Update exec.56.py

--- exec.56.py
+++ exec.56.py
@@ -1,2 +1,15 @@
 #Maior e menor da sequência
 #Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+maior = 0
+menor = 0
+for p in range(1, 6):
+    peso = float(input('Peso da {} pessoa: '.format(p)))
+    if p == 1:
+        maior = p
+        menor = p
+    else:
+        if peso > maior:
+            maor = peso
+        if peso < menor:
+            menor = peso
+            
