8683eb692ae1a4d7bfe9995176c38a021643634d
Domingosws <domingosw1991@gmail.com>
2022-05-04 17:41:40 -0300

    Update exec.56.py

--- exec.56.py
+++ exec.56.py
@@ -5,11 +5,12 @@ menor = 0
 for p in range(1, 6):
     peso = float(input('Peso da {} pessoa: '.format(p)))
     if p == 1:
-        maior = p
-        menor = p
+        maior = peso
+        menor = peso
     else:
         if peso > maior:
             maor = peso
         if peso < menor:
             menor = peso
-            
+print('O maior peso lido foi de {} Kg'.format(maior))
+print('O menor peso lido foi de {} Kg'.format(menor))
