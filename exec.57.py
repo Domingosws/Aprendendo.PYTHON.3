15da3c2be5c73590087277199f49f64acd85a8e7
Domingosws <domingosw1991@gmail.com>
2022-05-06 00:45:13 -0300

    Update exec.57.py
    Analisador de  nomes.

--- exec.57.py
+++ exec.57.py
@@ -1,9 +1,26 @@
 #Analisador completo
 # Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas.
 # No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+somaidade = 0
+médiaidade = 0
+maioridadehomem = 0
+nomevelho = ""
+totmulher20 = 0
 for p in range(1, 5):
     print('------- {} PESSOA -------'.format(p))
     nome = str(input('Nome:')).strip()
     idade = int(input('Idade:'))
     sexo = str(input('Sexo [M/F]: ')).strip()
-    
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+médiaidade = somaidade / 4
+print('Media de idade do grupo é de {} anos!'.format(médiaidade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(totmulher20))
