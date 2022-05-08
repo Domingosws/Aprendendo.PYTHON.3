266bb374b150061fcd5fc022f099dda499a82cff
Domingosws <domingosw1991@gmail.com>
2022-05-08 20:29:50 -0300

    Update exec.58.py

--- exec.58.py
+++ exec.58.py
@@ -1,2 +1,4 @@
 #Repetições em Python (while)
 #Validação de Dados
+#Exercício Python 57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
+#Caso esteja errado, peça a digitação novamente até ter um valor correto.
