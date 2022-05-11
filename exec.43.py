213adada04ebbe1ed181a8ce50b53e39abfe5b3b
Domingosws <domingosw1991@gmail.com>
2022-05-11 00:07:00 -0300

    Update exec.43.py

--- exec.43.py
+++ exec.43.py
@@ -3,16 +3,16 @@
 # – EQUILÁTERO: todos os lados iguais
 # – ISÓSCELES: dois lados iguais, um diferente
 # – ESCALENO: todos os lados diferentes
-r1 = float(input('Primeiro segmento: '))
-r2 = float(input('Segundo segmento: '))
-r3 = float(input('Terceiro segmento: '))
+r1 = float(input('\033[1;34mPrimeiro segmento: \033[m'))
+r2 = float(input('\033[1;34mSegundo segmento: \033[m'))
+r3 = float(input('\033[1;34mTerceiro segmento: \033[m'))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 +r2:
-    print('Os segmentos acimam PODEM FARMAR um triagulo', end=' ')
+    print('\033[1;34mOs segmentos acimam PODEM FARMAR um triagulo\033[m', end=' ')
     if r1 == r2 == r3:
-        print('EQUILATERO!')
+        print('\033[1;36mEQUILATERO!\033[m')
     elif r1 != r2 != r3!= r1:
-        print('ESCALENO!')
+        print('\033[1;36mESCALENO!\033[m')
     else:
-        print('ISÓSCELES')
+        print('\033[1;36mISÓSCELES\033[m')
 else:
-    print('Os segmentos acima NÃO PODEM FORMA um triagulo')
+    print('\033[1;31mOs segmentos acima NÃO PODEM FORMA um triagulo\033[m')
