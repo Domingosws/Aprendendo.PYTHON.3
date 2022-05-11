ece80fe5fa81d3d16b5a779e34018bb9749bc44d
Domingosws <domingosw1991@gmail.com>
2022-05-10 23:53:21 -0300

    Update exec.44.py

--- exec.44.py
+++ exec.44.py
@@ -6,17 +6,17 @@
 #– 25 até 30: Sobrepeso
 #– 30 até 40: Obesidade
 #– Acima de 40: Obesidade Mórbida
-peso = float(input('Qual é seu peso? (Kg)'))
-altura = float(input('Qual é sua altura? (m)'))
+peso = float(input('\033[1;32mQual é seu peso? (Kg)\033[m'))
+altura = float(input('\033[1;32mQual é sua altura? (m)\33[m'))
 imc = peso / (altura ** 2)
-print('O IMC dessa passoa é de {:.1f}'.format(imc))
+print('\033[1;37mO IMC dessa pessoa é de {:.1f}\033[m'.format(imc))
 if imc < 18.5:
-    print('Você está ABAIXO DO PESO normal')
+    print('\033[1;31mVocê está ABAIXO DO PESO normal\033[m')
 elif 18.5 <= imc < 25:
-    print('PARABÊNS, você está na faixa de peso NORMAL')
+    print('\033[1;37mPARABÊNS, você está na faixa de peso NORMAL\033[m')
 elif 25<= imc < 30:
-    print('Você está em SOPREPESO')
+    print('\033[1;37mVocê está em SOPREPESO\033[m')
 elif 30 <= imc < 40:
-    print('Você está em OBESIDADE!')
+    print('\033[1;37mVocê está em OBESIDADE!\033[m')
 elif imc >=40:
-    print('Você está em OBESIDADE MÓRBIDA, CUIDADO!')
+    print('\033[1;31mVocê está em OBESIDADE MÓRBIDA, CUIDADO!\033[m')
