4255bc7ff0599cf69d8b26d45d221d6a5b326d7f
Domingosws <domingosw1991@gmail.com>
2022-05-24 23:48:36 -0300

    Update exec.58.py

--- exec.58.py
+++ exec.58.py
@@ -2,7 +2,7 @@
 #Validação de Dados
 #Exercício Python 57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
 #Caso esteja errado, peça a digitação novamente até ter um valor correto.
-sexo = str(input('Informe seu sexo: [M/F]')).strip().upper()[0]
+sexo = str(input('\033[1;34mInforme seu sexo: [M/F]\033[m')).strip().upper()[0]
 while sexo not in 'MmFf':
     sexo = str(input('Dados invalidos. Por  favor, informe seu sexo ')).strip().upper()[0]
 print('Sexo {} registrado com sucesso'.format(sexo))
