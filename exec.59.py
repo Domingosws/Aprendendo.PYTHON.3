0fd02e396a247dc494f1658c5021a54f2a320741
Domingosws <domingosw1991@gmail.com>
2022-05-28 22:20:56 -0300

    Update exec.59.py

--- exec.59.py
+++ exec.59.py
@@ -2,5 +2,14 @@
 #Só que agora o jogador vai tentar adivinhar até acertar,
 #mostrando no final quantos palpites foram necessários para vencer.
 from random import randint
-computador = ramdint(0, 10)
-print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+computador = randint(0, 10)
+print('\033[1;34mSou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivimhar qual foi?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite?'))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+print('\033[1;31mAcertou com {} palpites!\033[m'.format(palpites))
