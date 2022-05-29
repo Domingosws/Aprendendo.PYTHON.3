a281dfafd807d7b7506d7db841bf74cfd2bc8ce3
Domingosws <domingosw1991@gmail.com>
2022-05-28 22:01:29 -0300

    Update exec.59.py

--- exec.59.py
+++ exec.59.py
@@ -1,4 +1,6 @@
 #Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10.
 #Só que agora o jogador vai tentar adivinhar até acertar,
 #mostrando no final quantos palpites foram necessários para vencer.
-#fazendo
+from random import randint
+computador = ramdint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
