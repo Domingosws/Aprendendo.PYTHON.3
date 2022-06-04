a570aa5dd4c36c7e503c4ef36eb6a959b35081a5
Domingosws <domingosw1991@gmail.com>
2022-06-03 23:56:11 -0300

    Update exec.60.py

--- exec.60.py
+++ exec.60.py
@@ -6,12 +6,14 @@
 #[ 4 ] novos números
 #[ 5 ] sair do programa
 #Seu programa deverá realizar a operação solicitada em cada caso.
-n1 = int(input('Primeiro valor: '))
-n2 = int(input('Segundo valor: '))
-
-print('''[1] Somar
-[2] Multiplicar
-[3] Maior
-[4] Novos números
-[5] Sair do programa''')
-opção = str(input('Qual é sua opção? '))
+n1 = int(input('\033[1;36mPrimeiro valor: '))
+n2 = int(input('Segundo valor:\033[m '))
+opção = 0
+while opção != 5:
+    print('''\033[1;33m    [1] Somar
+    [2] Multiplicar
+    [3] Maior
+    [4] Novos números   
+    [5] Sair do programa\033[m''')
+    opção = int(input('Qual é sua opção? '))
+print('\033[1;37mFim do programa!\033[m')
