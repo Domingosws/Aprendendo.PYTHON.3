beedfd09bc8570ef2d6b2e8ae80b12add13a1e7b
Domingosws <domingosw1991@gmail.com>
2022-06-04 00:39:42 -0300

    Update exec.60.py

--- exec.60.py
+++ exec.60.py
@@ -15,5 +15,26 @@ while opção != 5:
     [3] Maior
     [4] Novos números   
     [5] Sair do programa\033[m''')
-    opção = int(input('Qual é sua opção? '))
+    opção = int(input('\033[1;32mQual é sua opção?\033[m '))
+    if opção == 1:
+        soma = n1 + n2
+        print('\33[1;35mA soma entre {} + {} é {}\033[m'.format(n1, n2, soma))
+    elif opção == 2:
+        produto = n1 * n2
+        print('\033[1;35mO resutado de {} x {} é {}\033[m'.format(n1, n2, produto))
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print('\033[1;35mEntre {} e {} o maior valor é {}\033[m'.format(n1, n2, maior))
+    elif opção == 4:
+        print('\033[1;35mInforme os números novamente:\033[m')
+        n1 = int(input('\033[1;33mPrimerio valor\033[m'))
+        n2 = int(input('\033[1;33mSegundo valor:\033[m'))
+    elif opção == 5:
+        print('\033[1;35mFinalizado...\033[m')
+    else:
+        print('\033[1;31mOpção inválida. Tente novamente\033[m')
+        print('--'* 10)
 print('\033[1;37mFim do programa!\033[m')
