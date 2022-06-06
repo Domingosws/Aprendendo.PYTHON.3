558eeafce43d5cc7ae9ccdd1d6a2b1ecef81df3b
Domingosws <domingosw1991@gmail.com>
2022-06-06 14:23:17 -0300

    Update exec.61.py

--- exec.61.py
+++ exec.61.py
@@ -1,8 +1,8 @@
-#Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
-#5! = 5 x 4 x 3 x 2 x 1 = 120
-
+# CÁLCULO DO FATORIAL
+# Faça um programa que leia um número qualquer e mostre o seu fatorial.
 from math import factorial
-n =int(input('Digite um número para clcular seu Fatorial: '))
+
+n = int(input('Digite um número para cálcular seu Fatorial: '))
 f = factorial(n)
 print('O fatorial de {} é {}.'.format(n, f))
-#Fim de algoritimo
+# Fim de algoritimo.
