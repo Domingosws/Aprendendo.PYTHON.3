7814740b584ed570b3330fb546ea6fdd99db103c
Domingosws <domingosw1991@gmail.com>
2022-06-10 23:10:16 -0300

    Update exec.62.py

--- exec.62.py
+++ exec.62.py
@@ -1,3 +1,3 @@
 #Progressão Aritmética v2.0
 #lendo o primeiro termo e a razão de uma PA,
-#mostrando os 10 primeiros termos da progressão usando a estrutura while.
+#mostrando os 10 primeiros termos da progressão usando a estrutura while
