93b19bad544e9da1e21001dcd7bff9e3b0e52b40
Domingosws <domingosw1991@gmail.com>
2022-06-11 23:10:02 -0300

    Update exec.62.py

--- exec.62.py
+++ exec.62.py
@@ -1,3 +1,8 @@
 #Progressão Aritmética v2.0
 #lendo o primeiro termo e a razão de uma PA,
 #mostrando os 10 primeiros termos da progressão usando a estrutura while
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+cont = 1
