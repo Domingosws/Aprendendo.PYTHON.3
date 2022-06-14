42a4945bfece5d3e36d3bb173219e9556daca614
Domingosws <domingosw1991@gmail.com>
2022-06-13 23:52:27 -0300

    Update exec.63.py

--- exec.63.py
+++ exec.63.py
@@ -1,3 +1,3 @@
 #Super Progressão Aritmética v3.0 Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
-# O programa encerrará quando ele disser que quer mostrar 0 termos
+# O programa encerrará quando ele disser que quer mostrar 0 termos.
 
