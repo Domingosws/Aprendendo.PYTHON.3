63287d453fd775fe03e306adfede412bcf8780dd
Domingosws <domingosw1991@gmail.com>
2022-07-12 23:55:27 -0300

    Update exec.63.py

--- exec.63.py
+++ exec.63.py
@@ -6,8 +6,14 @@ primeiro = int(input('Primeiro termo: '))
 razão = int(input('Razão da PA: '))
 termo = primeiro
 cont = 1
-while cont <= 10:
-    print('{} ➮ '.format(termo), end='')
-    termo += razão
-    cont += 1
-print('FIM !')
+total = 0
+mais = 10
+while mais != 0
+    total = total + mais
+    while cont <= total:
+        print('{} ➮ '.format(termo), end='')
+        termo += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('FIM')
