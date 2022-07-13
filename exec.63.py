f46f279f61739ea88efc4655d34d74d8a527721d
Domingosws <domingosw1991@gmail.com>
2022-07-13 00:08:11 -0300

    Update exec.63.py

--- exec.63.py
+++ exec.63.py
@@ -8,7 +8,7 @@ termo = primeiro
 cont = 1
 total = 0
 mais = 10
-while mais != 0
+while mais != 0:
     total = total + mais
     while cont <= total:
         print('{} ➮ '.format(termo), end='')
@@ -16,4 +16,5 @@ while mais != 0
         cont += 1
     print('PAUSA')
     mais = int(input('Quantos termos você quer mostrar a mais? '))
-print('FIM')
+print('Progressão finalizada com {} termos mostrados.'.format(total))
+
