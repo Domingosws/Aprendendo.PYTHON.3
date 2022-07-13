00f039ac650dc8f9989740d5631704e29f57f7e6
Domingosws <domingosw1991@gmail.com>
2022-07-12 23:43:00 -0300

    Update exec.63.py

--- exec.63.py
+++ exec.63.py
@@ -1,3 +1,13 @@
 #Super Progressão Aritmética v3.0 Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
 # O programa encerrará quando ele disser que quer mostrar 0 termos.
-# fazendo...
+print('   Gerador de PA')
+print('☆' * 15)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} ➮ '.format(termo), end='')
+    termo += razão
+    cont += 1
+print('FIM !')
