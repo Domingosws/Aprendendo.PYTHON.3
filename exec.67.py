66a729a6d7e99b7b8d1905dcdb143666c5ba8151
Domingosws <86332803+Domingosws@users.noreply.github.com>
2022-08-24 15:30:06 -0300

    Create exec.67.py

---
+++ exec.67.py
@@ -0,0 +1,5 @@
+#INTERROMPENDO REPETIÇÕES WHILE
+#Nessa aula, vamos  aprender como utilizar a instrução break e os loopings infinitos a favor das nossas estratégias de código.
+#É muito importante saber usar o break no Pyton,já que em alguns casos precisamos interromper um laço no caminho.
+#Além disso, vamos aprender como trabalhar com as novas fstrngs do python.
+ 
