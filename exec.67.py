20f8e2e6460ce00c57bfc0d83fddcbffcae71d15
Domingosws <86332803+Domingosws@users.noreply.github.com>
2022-08-28 20:51:49 -0300

    Update exec.67.py

--- exec.67.py
+++ exec.67.py
@@ -1,4 +1,11 @@
 #INTERROMPENDO REPETIÇÕES WHILE
 #Nessa aula, vamos  aprender como utilizar a instrução break e os loopings infinitos a favor das nossas estratégias de código.
 #É muito importante saber usar o break no Pyton,já que em alguns casos precisamos interromper um laço no caminho.
-#Além disso, vamos aprender como trabalhar com as novas fstrngs do python.
+#Além disso, vamos aprender como trabalhar com as novas fstrngs do pyth
+n= s =0
+while True:
+    n=int(input('digite um numero'))
+    if n == 999:
+        break
+    s += n
+print(f'a soma {s} {n}')
