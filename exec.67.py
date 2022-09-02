9578689c6bc5e2165c856adecac1ed89733ee511
Domingosws <86332803+Domingosws@users.noreply.github.com>
2022-09-01 22:41:42 -0300

    Update exec.67.py

--- exec.67.py
+++ exec.67.py
@@ -2,7 +2,7 @@
 # Nessa aula, vamos aprender como utilizar a instrução break e os ‘loopings’ infinitos a favor das nossas estratégias de código.
 # É muito importante saber usar o break no Pyton, já que em alguns casos precisamos interromper um laço no caminho.
 # Além disso, vamos aprender como trabalhar com as novas fstrngs do python.
-
+#------------------------------------------------------------------------------------------------------------------------
 # Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que
 # é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag)
 
