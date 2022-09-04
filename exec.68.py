31188bb377f1b2fe6772a8f3641888ebacff5262
Domingosws <86332803+Domingosws@users.noreply.github.com>
2022-09-03 22:56:47 -0300

    Update exec.68.py

--- exec.68.py
+++ exec.68.py
@@ -2,3 +2,6 @@
 #Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999,
 # que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
 
+
+
+
