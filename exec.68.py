4b24ad02e0874318916cb561c8c2eea7bc25756c
Domingosws <86332803+Domingosws@users.noreply.github.com>
2022-09-05 16:23:07 -0300

    Update exec.68.py

--- exec.68.py
+++ exec.68.py
@@ -1,7 +1,12 @@
-#Vários números com flag
-#Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999,
-# que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
-
-
-
-
+#Tabuada v3.0
+# Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
+# O programa será interrompido quando o número solicitado for negativo.
+while True:
+    n = int(input('Quer ver a tanuada de qual valor? '))
+    if n < 0:
+        break
+    print('-' * 30)
+    for c in range(1, 11):
+        print(f'{n} x {c} = {n * c}')
+    print('-' * 30)
+print('PROGRAMA DA TABUADA ENCERRADO')
