2d9f8fb892fa8dfd74d9a947a8892ee0a67fbdb7
Domingosws <86332803+Domingosws@users.noreply.github.com>
2022-09-05 17:58:56 -0300

    Update exec.69.py
    jogo do par ou ímpar

--- exec.69.py
+++ exec.69.py
@@ -2,9 +2,29 @@
 # mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 
 from random import randint
+v = 0
 while True:
-    jogador = int(input('Diga um valor: '))
+    jogador = int(input('\033[1;34mDiga um valor:\033[m'))
     computador = randint(0, 10)
     total = jogador + computador
-    print(f'Voce jogou {jogador} e o computador {computador}.')
-    
+    tipo =''
+    while tipo not in 'Pp Ii':
+        tipo = str(input('Par ou Impar?')).strip().upper()[0]
+    print(f'\033[1;34mVoce jogou {jogador} e o computador {computador}. total de {total} ', end='')
+    print('Deu  PAR' if total % 2 == 0 else 'Deu IMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    print('\033[1;34mVamos jogar novamente...')
+    print(f'GAME OVER! Você venceu {v} vezes.\033[m')
